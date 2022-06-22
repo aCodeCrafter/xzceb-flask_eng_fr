2008d53da48ee685194daef8941b13c67710306b
Liam C <aCodeCrafter@gmail.com>
2022-06-22 17:59:58 +0000

    Implement translation package and HTML rendering in server.py

--- final_project/server.py
+++ final_project/server.py
@@ -1,5 +1,6 @@
 from machinetranslation import translator
 from flask import Flask, render_template, request
+import machinetranslation
 import json
 
 app = Flask("Web Translator")
@@ -8,17 +9,22 @@ app = Flask("Web Translator")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to French"
+    translated_text = machinetranslation.translator.englishToFrench(textToTranslate)
+    return translated_text
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to English"
+    translated_text = machinetranslation.translator.frenchToEnglish(textToTranslate)
+    return translated_text
 
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
-
+    # with open('templates/index.html') as html_file:
+    #     test = html_file.read()
+    return render_template('index.html')
+    # return test
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
